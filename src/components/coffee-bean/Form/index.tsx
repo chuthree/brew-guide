dfa51345c01697020eb3e6cf8d7ca47db9786e38
chuthree <chuthree01@gmail.com>
2025-09-29 22:14:35 +0800

    优化：移除添加咖啡豆的装饰条

--- src/components/coffee-bean/Form/index.tsx
+++ src/components/coffee-bean/Form/index.tsx
@@ -711,7 +711,7 @@ const CoffeeBeanForm: React.FC<CoffeeBeanFormProps> = ({
 
     return (
         <div className="flex flex-col">
-            <div className="flex items-center justify-between mt-3 mb-6">
+            <div className="flex items-center justify-between my-6">
                 <button type="button" onClick={handleBack} className="rounded-full">
                     <ArrowLeft className="w-5 h-5 text-neutral-800 dark:text-neutral-200" />
                 </button>
