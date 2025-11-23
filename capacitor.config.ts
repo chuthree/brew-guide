c365bd7b7683a68b70744a20422018f0b73542b6
chuthree <chuthree01@gmail.com>
2025-11-23 18:40:41 +0800

    移除：启动页

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,43 +1,33 @@
 /// <reference types="@capacitor-community/safe-area" />
 
-import type { CapacitorConfig } from "@capacitor/cli";
-import { KeyboardResize, KeyboardStyle } from "@capacitor/keyboard";
+import type { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
-	appId: "com.brewguide.app",
-	appName: "Brew Guide",
-	webDir: "out",
-	server: {
-		androidScheme: "https",
-		iosScheme: "https",
-		hostname: "app",
-	},
-	plugins: {
-		SplashScreen: {
-			launchShowDuration: 2000,
-			launchAutoHide: true,
-			backgroundColor: "#FFFFFF",
-			androidSplashResourceName: "splash",
-			androidScaleType: "CENTER_INSIDE",
-			showSpinner: false,
-			splashFullScreen: true,
-			splashImmersive: true,
-		},
-		Keyboard: {
-			resize: KeyboardResize.Native,
-			style: KeyboardStyle.Dark,
-			resizeOnFullScreen: true,
-		},
-		SafeArea: {
-			enabled: true,
-			customColorsForSystemBars: true,
-			statusBarColor: '#00000000', // 透明
-			statusBarContent: 'light',
-			navigationBarColor: '#00000000', // 透明
-			navigationBarContent: 'light',
-			offset: 0,
-		},
-	},
+  appId: 'com.brewguide.app',
+  appName: 'Brew Guide',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'https',
+    hostname: 'app',
+  },
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Native,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true,
+    },
+    SafeArea: {
+      enabled: true,
+      customColorsForSystemBars: true,
+      statusBarColor: '#00000000', // 透明
+      statusBarContent: 'light',
+      navigationBarColor: '#00000000', // 透明
+      navigationBarContent: 'light',
+      offset: 0,
+    },
+  },
 };
 
 export default config;
